31966d1248a836886869a9dca93023c1adf2db76
Efen Stakes <efenstakes101@gmail.com>
2021-11-05 16:54:40 +0300

    add app navigation items

--- grocery_shop_amarullahs/ContentView.swift
+++ grocery_shop_amarullahs/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                
+                
+                
+                
+                
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(
+                leading:
+                    Image(systemName: "chevron.left")
+                    .resizable()
+                    .frame(width: 10, height: 20, alignment: .center),
+                trailing:
+                    Image(systemName: "bag")
+                    .resizable()
+                    .frame(width: 16, height: 20, alignment: .center)
+                    .padding(10)
+                    .overlay(
+                        Circle().stroke(Color.black, lineWidth: 1)
+                    )
+            )
+        }
     }
 }
 
