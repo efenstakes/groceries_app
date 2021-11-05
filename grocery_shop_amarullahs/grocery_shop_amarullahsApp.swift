8716c7d4fd44e5b26cb6044c4be6e537799e9637
Efen Stakes <efenstakes101@gmail.com>
2021-11-05 16:32:14 +0300

    Initial Commit

---
+++ grocery_shop_amarullahs/grocery_shop_amarullahsApp.swift
@@ -0,0 +1,17 @@
+//
+//  grocery_shop_amarullahsApp.swift
+//  grocery_shop_amarullahs
+//
+//  Created by MAC on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct grocery_shop_amarullahsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ grocery_shop_amarullahsUITests/grocery_shop_amarullahsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  grocery_shop_amarullahsUITestsLaunchTests.swift
+//  grocery_shop_amarullahsUITests
+//
+//  Created by MAC on 11/5/21.
+//
+
+import XCTest
+
+class grocery_shop_amarullahsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
